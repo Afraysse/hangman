21963126ae0fd83fe9ebb4d19e71d303663e02a1
Anne Fraysse <AnneFraysse@Annes-MacBook-Pro.local>
2016-07-01 09:59:46 -0700

    Changed the function structure and added a conclusion; fixed loop

--- hangman.py
+++ hangman.py
@@ -1,54 +1,80 @@
-    
+"""Hangman"""
+
 import random
 
+guesses_allowed = 8
+fail_counter = 0
+letter_list = []
+
+
 def hangman():
+    global fail_counter
+    global letter_list
 
-    """ A simple python based hangman game that loops over a list of words. The word used
-    is selected randomly and the letters are replaced by the .replace() function in 
-    python. Would like to incorperate Angular JS. Enjoy!
+    dead = '''
+      _______
+     |\      |
+     |      (_)
+     |      \|/
+     |       |
+     |      L L
+     |
+ jgs_|___
+ '''
 
-    """
-    
-    guess_counter = 0
-    fail_counter = 0
-    letter_list = []
-
-    
-    WORDS = [ "fishsticks", "cartman", "imaginationland", "kenny", "south park", 
-            "garrison", "randy", "stan marsh", "new kid", "colorado", 
-            "make love not warcraft", "trapped in the closet", "cheesin" ]
-    
+    WORDS = ["fishsticks", "cartman", "imaginationland", "kenny", "garrison", "randy", "colorado", "cheesin"]
     word = random.choice(WORDS)
-    
-    i = 0
-    while i < len(word):
-        for char in word:
-            line = word.replace(word, ' _ ')
-            line_spaces = line * len(word)
-            
-            i += 1
-    print line_spaces
-    letter = raw_input("Please guess a letter: ")
-    
-    if len(letter) == 1 and letter.isalpha:
-    
-    # user inputs letter, check letter
-        letter = False
-        while not letter:
-        
-        
-            if letter == char in word:
-                letter_change = word.replace(char, letter)
-                print "Success!"
-                correct = True
-                letter_list.append(letter)
-                guess_counter += 1
-            
-            else:
-                print "Incorrect! Please guess again"
-                letter_again = raw_input("Guess: ") 
-                fail_counter += 1
-            
+    finished = printit(word, '')
+
+    while finished is not True:
+
+        is_a_letter = False
+        letter = ''
+        while is_a_letter is False:
+            letter = raw_input("Please guess a letter: ")
+            is_a_letter = len(letter) == 1 and letter.isalpha
+
+        letter_list.append(letter)
+
+        finished = printit(word, letter)
+        print " "
+        print "Failed guesses: ", fail_counter
+
+        if fail_counter == guesses_allowed:
+            print dead
+            break
+
+    if finished:
+        print "You win!!"
+    else:
+        print "You lost! The word was: " + word
+
+    again = raw_input("Do you want to play again? (y/n) ")
+    if again == 'y':
+        fail_counter = 0
+        letter_list = []
+        hangman()
+
+
+def printit(word, guess):
+    global fail_counter
+    finished = True
+    letter_failed = True
+    result = ''
+    for letter in word:
+        if guess == letter or guess == '':
+            letter_failed = False
+        if letter in letter_list:
+            result += " " + letter + " "
+        else:
+            finished = False
+            result += " _ "
+    if letter_failed:
+        fail_counter += 1
+
+    print result
+    return finished
+
 
 hangman()
         
